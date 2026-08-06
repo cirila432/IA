222e03cbe6fcda98c2ee0eedaa3ba962b8e8f990
cirila432 <wandoski.priscila@escola.pr.gov.br>
2026-08-06 13:11:20 -0300

    dd

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alterntivas")
 const caixaResultados = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
+
+const perguntas =[
+{
+   enunciado: "Pergunta 1", 
+   alternativas:
+   ["Alternativa 1",
+    Alternativa 2"
+    ] 
+   }
+   {
+   enunciado: "Pergunta 1", 
+   alternativas:
+   ["Alternativa 1",
+    Alternativa 2"
+    ] 
+   }
